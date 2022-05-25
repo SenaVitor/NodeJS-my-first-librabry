5ff24ea0d7179e55dae386c4cd22a53f8a0c45ed
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-05-25 20:29:31 -0300

    Create a function to print a text file

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 const chalk = require('chalk');
-console.log(chalk.red.bold('Hello World'));
+const fs = require('fs');
 
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string){
-    return string;
+function pegaArquivo(caminho){
+    const encoding = 'utf-8';
+    fs.readFile(caminho, encoding, (_, texto) => {
+        console.log(chalk.green(texto));
+    });
 }
 
-console.log(texto(paragrafo));
-
-//encadear métodos para colorir texto, cor de fundo e texto em negrito
-console.log(chalk.blue.bgWhite.bold('Alura'));
-
-//receber múltiplos argumentos
-console.log(chalk.blue('curso', 'de', 'NodeJS'));
-
-//métodos aninhados
-console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
-
-// uso de template strings e placeholders
-console.log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+pegaArquivo('./arquivos/texto1.md');
