a1da6891d22cf4b4f2acd59ceabab61b321797fd
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-05-26 21:02:45 -0300

    Test for erros in the code and create a function to return it

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
+}
+
 function pegaArquivo(caminho){
     const encoding = 'utf-8';
-    fs.readFile(caminho, encoding, (_, texto) => {
+    fs.readFile(caminho, encoding, (erro, texto) => {
+        if(erro){
+            trataErro(erro);
+        }
         console.log(chalk.green(texto));
     });
 }
