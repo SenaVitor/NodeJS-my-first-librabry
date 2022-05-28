e2b57d37f1179a30b32da940b1615f0c340a3ba4
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-05-28 09:44:35 -0300

    Create a function to print links and their names with regex

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
+
+function extraiLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const linksExtraidos = texto.match(regex);
+    console.log(linksExtraidos);
+}
+
+extraiLinks(texto);
+
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
@@ -33,4 +43,4 @@ async function pegaArquivo(caminho){
 //     });
 // }
 
-pegaArquivo('./arquivos/texto1.md');
+//pegaArquivo('./arquivos/texto1.md');
