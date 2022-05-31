d75b44c0a44a23f7abe98ea4dfc78a6e4a11b88c
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-05-31 18:17:18 -0300

    Create a function to print the result of the pegaArquivo function

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 const pegaArquivo = require('./index');
 
-const caminho = process.argv;
+const caminho = process.argv; //Pega uma informação informada no terminal (Atualmente está sendo informada no arquivo package.json com o script cli)
 
 async function processaTexto(caminhoDeArquivo){
     const resultado = await pegaArquivo(caminho[2]);
