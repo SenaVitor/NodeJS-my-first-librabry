776ba18a470c4996b49791e1310006ea404a89d0
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-05-31 17:38:07 -0300

    Create a function to print the result of pegaArquivo function

--- cli.js
+++ cli.js
@@ -1,5 +1,11 @@
+const chalk = require('chalk');
 const pegaArquivo = require('./index');
 
 const caminho = process.argv;
 
-console.log(pegaArquivo(caminho[2]));
+async function processaTexto(caminhoDeArquivo){
+    const resultado = await pegaArquivo(caminho[2]);
+    console.log(chalk.yellow('Lista de links'), resultado);
+}
+
+processaTexto(caminho);
