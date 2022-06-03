23d5e06c33b98c6ea3af9e73b010c095a5c91243
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-06-02 23:03:15 -0300

    Validate the links if informed by the user

--- cli.js
+++ cli.js
@@ -1,11 +1,16 @@
 const chalk = require('chalk');
 const pegaArquivo = require('./index');
+const validaUrls = require('./http-validacao');
 
 const caminho = process.argv; //Pega uma informação informada no terminal (Atualmente está sendo informada no arquivo package.json com o script cli), além do caminho do node e do arquivo atual.
 
 async function processaTexto(caminhoDeArquivo){
     const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('Lista de links'), resultado);
+    if(caminho[3] === 'validar'){
+        console.log(chalk.yellow('links validados'), validaUrls(resultado));    
+    }else{
+        console.log(chalk.yellow('Lista de links'), resultado);
+    }
 }
 
 processaTexto(caminho);
