926c4326a5e9d0f38f5cd11156f2a7767a5194f3
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-06-05 10:16:26 -0300

    Create a function to make requests to links and return their status

--- http-validacao.js
+++ http-validacao.js
@@ -1,9 +1,21 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+async function checaStatus(arrayURLs){
+    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
+        const res = await fetch(url);
+        return res.status;
+    }));
+    return arrayStatus;
+}
+
 function geraArraysDeUrls(arrayLinks){
     return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
 }
 
-function validaUrls(arrayLinks){
-    return geraArraysDeUrls(arrayLinks);
+async function validaUrls(arrayLinks){
+    const links = geraArraysDeUrls(arrayLinks);
+    const statusLinks = await checaStatus(links);
+    return statusLinks;
 }
 
 module.exports = validaUrls;
