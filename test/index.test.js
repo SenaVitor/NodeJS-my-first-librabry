b5ac3c24ee0064a6f015437ed9779fba8d1684d9
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-06-05 12:37:24 -0300

    Run tests using jest

--- test/index.test.js
+++ test/index.test.js
@@ -14,4 +14,11 @@ describe('pegaArquivo::', () => {
         const resultado = await pegaArquivo('/home/vitor/Documentos/cursos/node_primeira_biblioteca/test/arquivos/texto1.md');
         expect(resultado).toEqual(arrayResult);
     })
+    it('deve retornar a mensagem "não há links"', async () => {
+        const resultado = await pegaArquivo('/home/vitor/Documentos/cursos/node_primeira_biblioteca/test/arquivos/texto1_semlinks.md');
+        expect(resultado).toEqual('não há links');
+    })
+    it('deve lançar um erro na falta de arquivo', async () => {
+        await expect(pegaArquivo('/home/vitor/Documentos/cursos/node_primeira_biblioteca/test/arquivos')).rejects.toThrow(/não há arquivo no caminho/)
+    })
 })
