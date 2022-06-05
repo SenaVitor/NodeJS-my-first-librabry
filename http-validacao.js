29a7c806bca254a8f16afa03e6283d1f0ac90de1
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-06-05 11:20:24 -0300

    Return an array with the links, their status and descriptions

--- http-validacao.js
+++ http-validacao.js
@@ -10,7 +10,7 @@ async function checaStatus(arrayURLs){
             .all(arrayURLs
                 .map(async url => {
                     const res = await fetch(url);
-                    return res.status;
+                    return `${res.status} - ${res.statusText}`;
             }));
         return arrayStatus;
     }catch(erro){
